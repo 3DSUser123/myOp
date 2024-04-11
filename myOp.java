d4523f657bfa05b74c3d5372e14bb6a0ee0ed2c5
3DSUser123 <166298354+3DSUser123@users.noreply.github.com>
2024-04-11 20:34:05 +0900

    Add files via upload

---
+++ myOp.java
@@ -0,0 +1,31 @@
+package New;
+
+public class myOp {
+
+	public static void main(String[] args) {
+		//result 값
+		int result = 1+2;
+		System.out.println(result);
+		
+		//result 값 2 (-는 결과 값을 빼기)
+		result = result - 1;
+		System.out.println(result);
+		
+		//result 값 6 (*은 -의 결과 값을 곱하기)
+				result = result * 3;
+				System.out.println(result);
+				
+		//result 값 3 (/은 *의 결과 값을 나누기)
+				result = result / 2;
+				System.out.println(result);
+				
+		//result 값 10 (/값에서 7을 더해 결과값 나타내기)
+				result = result + 7;
+				System.out.println(result);
+		//result 값 3 (%로 나머지 값 구하기)
+				result = result % 7;
+				System.out.println(result);
+				
+	}
+
+}
